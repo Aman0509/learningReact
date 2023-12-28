5e795cd739da2b40a8c5f0ffc504719b51e44193
aman <amansaxena8055@gmail.com>
2023-12-28 15:23:38 +0530

    [Practice] restructure components in separate modules

--- 002-react-essentials/projects/01-starting-project/src/App.jsx
+++ 002-react-essentials/projects/01-starting-project/src/App.jsx
@@ -1,39 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
 import {CORE_CONCEPTS} from './data.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-// Generate random number within the range of `max`
-function genRandomInt(max){
-  return Math.floor(Math.random() * (max+1));
-}
-
-// Add Header component
-function Header(){
-  // adding values dynamically
-  const description = reactDescriptions[genRandomInt(2)]
-  return (
-    <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  );
-}
-
-// Add CoreConcept Component
-function CoreConcept(props){ // alternatively, object destructuring can also be employed,for eg., `function CoreConcept ({image, title, description})`
-  return (
-    <li>
-      <img src={props.image} alt={props.description} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
 function App() {
   return (
---
+++ 002-react-essentials/projects/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import reactImg from '../assets/react-core-concepts.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+// Generate random number within the range of `max`
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max+1));
+}
+
+// Add Header component
+export default function Header(){
+	// adding values dynamically
+	const description = reactDescriptions[genRandomInt(2)]
+	return (
+		<header>
+				<img src={reactImg} alt="Stylized atom" />
+				<h1>React Essentials</h1>
+				<p>
+					{description} React concepts you will need for almost any app you are
+					going to build!
+				</p>
+			</header>
+	);
+}
