628e71868064ff36d9bd0a701b0b6cbab8805c6e
aman <amansaxena8055@gmail.com>
2023-12-30 19:48:00 +0530

    [Practice] `React.createElement`

--- 003-react-essentials-deep-dive/projects/01-starting-project/src/index.jsx
+++ 003-react-essentials-deep-dive/projects/01-starting-project/src/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
@@ -5,3 +6,6 @@ import "./index.css";
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
+
+// Example - If there is a need to replace JSX
+// ReactDOM.createRoot(entrypoint).render(React.createElement(App));
