c571ad19d55539a6f2319fc598ab977e20149016
aman <amansaxena8055@gmail.com>
2024-01-04 00:43:54 +0530

    [Practice] 4 things
    - keep all tic-tac-toe winning combinations into separate file to
      cater our logic
    - separate game board track logic from `GameBoard` component to
      `App` component
    - create logic for game
    - create `GameOver` component

--- 003-react-essentials-deep-dive/projects/02-tic-tac-toe-project/src/App.jsx
+++ 003-react-essentials-deep-dive/projects/02-tic-tac-toe-project/src/App.jsx
@@ -3,6 +3,14 @@ import { useState } from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/Gameboard.jsx";
 import Log from "./components/Log.jsx";
+import GameOver from "./components/GameOver.jsx";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -17,6 +25,34 @@ function App() {
   /* This seems to be an unnecessary state, hence commenting it for reference and created the function `deriveActivePlayer()` as the optimum way to go */
   // const [activePlayer, setActivePlayer] = useState("X");
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((currentPlayer) => (currentPlayer === "X" ? "O" : "X"));
@@ -48,7 +84,8 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {(winner || hasDraw) && <GameOver winner={winner} />}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
--- 003-react-essentials-deep-dive/projects/02-tic-tac-toe-project/src/components/GameBoard.jsx
+++ 003-react-essentials-deep-dive/projects/02-tic-tac-toe-project/src/components/GameBoard.jsx
@@ -1,35 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  /* Commenting this because we'll now define state at App.jsx level so that, it would be utilize by this and Log component. In this way, we can avoid intersecting states if, by chance, we lift up the state for Logs components which also maintain same data as used by this component */
-
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectSquare(rowIndex, colIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedGameBoard = [
-  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedGameBoard;
-  //   });
-  //   onSelectSquare();
-  // }
-
-  let gameBoard = initialGameBoard;
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-    gameBoard[row][col] = player;
-  }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
