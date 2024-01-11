604408c8345da54a0381249b486fff9effc4568d
aman <amansaxena8055@gmail.com>
2024-01-12 00:19:47 +0530

    [Practice] using refs to store values

--- 007-refs-and-portals/projects/01-starting-project/src/components/TimerChallenge.jsx
+++ 007-refs-and-portals/projects/01-starting-project/src/components/TimerChallenge.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
   const [timerExpired, setTimerExpired] = useState(false);
   const [timerStarted, setTimerStarted] = useState(false);
 
   function handleStart() {
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
     setTimerStarted(true);
   }
   function handleStop() {
-    // code
+    /* this method cancels a timeout previously established by calling setTimeout().
+    however, for that, it needs `timeoutID` returned by setTimeout()
+    in this case, setTimeout() used inside `handleStart` function
+		now, storing `timeoutID` in a variable does not work since due to state
+		change, react will re-trigger the component and along with variable will
+		also be reinitialized. To save our day, we can use refs to store `timeoutID`,
+		since on component retrigger, refs value will not be reset. Also, if this component is used at multiple places then every component will
+		get it's own individual ref variable */
+    clearTimeout(timer.current);
   }
   return (
     <section className="challenge">
@@ -21,7 +30,7 @@ export default function TimerChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
