6cca6666f88699e536d689d9ab1330c483b88338
aman <amansaxena8055@gmail.com>
2024-02-28 00:38:41 +0530

    [Practice] create & provide the context

---
+++ 009-advanced-state-management/projects/01-starting-project/src/store/shopping-cart-context.jsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+// naming this variable with starting letter in uppercase is a best practice or widely known convention
+// It's not mandatory but good to use
+export const CartContext = createContext({
+  items: [],
+});
