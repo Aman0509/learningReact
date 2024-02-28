dcf7b9a618edd7c4c2cc18bdbf89eae1056143e6
aman <amansaxena8055@gmail.com>
2024-02-29 00:57:21 +0530

    [Practice] migrate context & state into a separate provider component

--- 009-advanced-state-management/projects/01-starting-project/src/store/shopping-cart-context.jsx
+++ 009-advanced-state-management/projects/01-starting-project/src/store/shopping-cart-context.jsx
@@ -1,4 +1,6 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+
+import { DUMMY_PRODUCTS } from "../dummy-products";
 
 // naming this variable with starting letter in uppercase is a best practice or widely known convention
 // It's not mandatory but good to use
@@ -7,3 +9,80 @@ export const CartContext = createContext({
   addItemToCart: () => {},
   updateItemQuantity: () => {},
 });
+
+export default function CartContextProvider({ children }) {
+  const [shoppingCart, setShoppingCart] = useState({
+    items: [],
+  });
+
+  function handleAddItemToCart(id) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+
+      const existingCartItemIndex = updatedItems.findIndex(
+        (cartItem) => cartItem.id === id
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+        updatedItems.push({
+          id: id,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+        });
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === productId
+      );
+
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+
+      updatedItem.quantity += amount;
+
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  // Linking the state in context:
+  // Ensure that along with passing the state in context,
+  // one should also pass functions or code in the context which
+  // is responsible for updating state. Rather than, passing all
+  // via props. And this ensures the complete usage of context API
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
+  };
+
+  return (
+    <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
+  );
+}
