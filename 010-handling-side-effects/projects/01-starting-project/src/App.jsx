7ae3c68ee4184caf1f19734d22f45f7b1ea67235
aman <amansaxena8055@gmail.com>
2024-03-05 23:37:21 +0530

    [Practice] not all side effects need `useEffect`

--- 010-handling-side-effects/projects/01-starting-project/src/App.jsx
+++ 010-handling-side-effects/projects/01-starting-project/src/App.jsx
@@ -7,11 +7,31 @@ import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
 import { sortPlacesByDistance } from "./loc.js";
 
+const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+const storedPlaces = storedIds.map((id) =>
+  AVAILABLE_PLACES.find((place) => place.id === id)
+);
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+
+  // purpose: to set the previously selected places again if page is reload
+  // here, the usage of `useEffect` is redundant because using it make more sense
+  // when you have async code, like fetching data from API etc,. However, this is
+  // a synchronous code, we can skip the usage of `useEffect` and place it in a way
+  // so that this side effect does not cause any unexpected behavior.
+  // A good approach is to keep this side effect out of the App component and initialized
+  // `pickedPlaces` state with `storedPlaces`
+  // useEffect(() => {
+  //   const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+  //   const storedPlaces = storedIds.map((id) =>
+  //     AVAILABLE_PLACES.find((place) => place.id === id)
+  //   );
+  //   setPickedPlaces(storedPlaces);
+  // }, []);
 
   // this is a side effect
   // unexpected behavior with this approach is infinite loop
@@ -45,6 +65,19 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    // creating a side effect to demo, "Not all side effects need useEffect"
+    // side effect: along with setting the picked place array via state, we also want to
+    // store this `pickedPlaces` array into browser's local storage.
+    // note - below piece of code is not directly related to rendering the JSX code below
+    // Hence, it's a side effect
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
@@ -52,6 +85,12 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    localStorage.setItem(
+      "selectedPlaces",
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    );
   }
 
   return (
