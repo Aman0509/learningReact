5d4bf3345bcc405fabae62200ed99a64c66a2a62
aman <amansaxena8055@gmail.com>
2024-03-07 17:22:05 +0530

    [Practice] prepare another case to utlize `useEffect`

--- 010-handling-side-effects/projects/01-starting-project/src/components/DeleteConfirmation.jsx
+++ 010-handling-side-effects/projects/01-starting-project/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,15 @@
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  // Demonstrate another case which can be handled with useEffect()
+  // introducing feature where this modal closes automatically after three seconds
+  // and automatically delete the place card. Basically, automatically confirming this modal after 3 secs
+
+  // now, this is a side effect
+  // this side effect needs to be handled with `useEffect`
+  // in order to clear the timer (if not, we will see unexpected behaviors)
+  setTimeout(() => {
+    onConfirm();
+  }, 3000);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
