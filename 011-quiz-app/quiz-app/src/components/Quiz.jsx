98f6c723aad70e903981406745217b599bfd8519
aman <amansaxena8055@gmail.com>
2024-03-10 17:18:03 +0530

    [Practice] derive values, output questions & register answers
    In React, you typically want to manage as little state as possible
    and derive as much state as possible.

--- 011-quiz-app/quiz-app/src/components/Quiz.jsx
+++ 011-quiz-app/quiz-app/src/components/Quiz.jsx
@@ -1,7 +1,36 @@
 import { useState } from "react";
+import QUESTIONS from "../../questions.js";
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  // in react, you typically want to manage as little state as possible and derive as
+  // much state as possible. Hence, state for `activeQuestionsIndex` is removed
   const [userAnswers, setUserAnswers] = useState([]);
-  return <p>Currently active Question</p>;
+
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswer) => {
+      return [...prevUserAnswer, selectedAnswer];
+    });
+  }
+  return (
+    <div id="quiz">
+      <div id="questions">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button
+                onClick={() => {
+                  handleSelectAnswer(answer);
+                }}
+              >
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
