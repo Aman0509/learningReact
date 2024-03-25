ebcee5bf2b3bac91cf3a5c5524e1b731398a7a3b
aman <amansaxena8055@gmail.com>
2024-03-26 00:02:41 +0530

    [Practice] sending HTTP request

--- 014-sending-http-requests/projects/01-starting-project/src/components/AvailablePlaces.jsx
+++ 014-sending-http-requests/projects/01-starting-project/src/components/AvailablePlaces.jsx
@@ -5,14 +5,25 @@ import Places from "./Places.jsx";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
+  // This is one way to fetch the data from endpoint
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/places")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((resData) => {
+  //       setAvailablePlaces(resData.places);
+  //     });
+  // }, []);
+
+  // We can also utilize async/await to fetch the data
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+    fetchPlaces();
   }, []);
 
   return (
