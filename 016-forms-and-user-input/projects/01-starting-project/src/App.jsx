5dfb15cf128b8e3152e9274428c12ce437dcefec
aman <amansaxena8055@gmail.com>
2024-07-23 13:45:03 +0530

    [Practice] validating input upon form submission

--- 016-forms-and-user-input/projects/01-starting-project/src/App.jsx
+++ 016-forms-and-user-input/projects/01-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header.jsx";
 // import SignUp from "./components/SignUp.jsx";
-import Login from "./components/StateLogin.jsx";
+// import Login from "./components/StateLogin.jsx";
+import Login from "./components/Login.jsx";
 
 function App() {
   return (
