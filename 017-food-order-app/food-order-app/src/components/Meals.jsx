22d48584347d3f27195bb14bc56a437fb593479f
aman <amansaxena8055@gmail.com>
2024-07-26 00:02:42 +0530

    [Practice] add `MealItem` component

--- 017-food-order-app/food-order-app/src/components/Meals.jsx
+++ 017-food-order-app/food-order-app/src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import MealItem from "./MealItem.jsx";
 
 export default function Meals() {
   const [loadedMeals, setLoadedMeals] = useState([]);
@@ -17,8 +18,8 @@ export default function Meals() {
 
   return (
     <ul id="meals">
-      {loadedMeals.map((meals) => (
-        <li id={meals.id}>{meals.name}</li>
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
       ))}
     </ul>
   );
