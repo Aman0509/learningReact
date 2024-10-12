c0b8873a1c459c9101b470c3908b1f26b1e33610
aman <amansaxena8055@gmail.com>
2024-10-12 22:47:00 +0530

    [Practice] add state slice

--- 018-diving-into-redux/projects/01-starting-project/src/store/index.js
+++ 018-diving-into-redux/projects/01-starting-project/src/store/index.js
@@ -1,9 +1,29 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   counter: 0,
   showTrue: true,
 };
+
+createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++; // here, we are mutating the state which we mentioned earlier not to do. However, redux toolkit uses `Immer` in background and maintain the required state immutability expected by redux
+    },
+    decrease(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter += action.amount;
+    },
+    toggleCounter(state) {
+      state.showTrue = !state.showTrue;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
