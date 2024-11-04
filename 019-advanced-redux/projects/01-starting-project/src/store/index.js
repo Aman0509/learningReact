dc95f578bbb18adffe83c0b186116878806afb61
aman <amansaxena8055@gmail.com>
2024-11-04 18:55:45 +0530

    [Practice] implement redux (part I)

---
+++ 019-advanced-redux/projects/01-starting-project/src/store/index.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import uiReducers from "./ui-slice";
+
+const store = configureStore({
+  reducer: {
+    ui: uiReducers,
+  },
+});
+
+export default store;
