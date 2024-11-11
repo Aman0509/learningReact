87a21f704dfe4974a4c3ff53b09932b5a82eb22a
aman <amansaxena8055@gmail.com>
2024-11-11 17:36:32 +0530

    [Practice] finalize fetch logic

--- 019-advanced-redux/projects/01-starting-project/src/store/cart-actions.js
+++ 019-advanced-redux/projects/01-starting-project/src/store/cart-actions.js
@@ -16,7 +16,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -42,7 +47,13 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch(
         "https://react-demo-backend-95950-default-rtdb.firebaseio.com/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
       );
       if (!response.ok) {
         throw new Error("Sending cart data failed!");
